4a538b4940ccbac051d0ac9018906196683c6a50
R0rt1z2 <me@r0rt1z2.com>
2024-11-10 18:52:54 +0100

    fs: proc: Override bootmode for offline charger

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,41 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/string.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#include <asm/setup.h>
+#include <mach/mt_boot.h>
+
+static char new_command_line[COMMAND_LINE_SIZE];
+
+static void append_to_cmdline(char *cmdline, const char *param)
+{
+    size_t len = strlen(cmdline);
+    size_t param_len = strlen(param);
+
+    if (len + param_len + 2 <= COMMAND_LINE_SIZE) {
+        if (len > 0 && cmdline[len - 1] != ' ') {
+            strcat(cmdline, " ");
+            len++;
+        }
+        strlcpy(cmdline + len, param, COMMAND_LINE_SIZE - len);
+    } else {
+        printk(KERN_WARNING "cmdline: Not enough space to append '%s'\n", param);
+    }
+}
+
+static void patch_charger_flags(char *cmdline)
+{
+    if (g_boot_reason == BR_USB) {
+        append_to_cmdline(cmdline, "androidboot.mode=charger");
+    }
+}
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -23,7 +54,17 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
-	return 0;
+    strlcpy(new_command_line, saved_command_line, sizeof(new_command_line));
+
+    /*
+     * Android automatically converts 'androidboot.*' properties to
+     * 'ro.boot.*' properties, since Amazon doesn't natively do this
+     * conversion, we do it here instead.
+     */
+    patch_charger_flags(new_command_line);
+    proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+
+    return 0;
 }
+
 module_init(proc_cmdline_init);
